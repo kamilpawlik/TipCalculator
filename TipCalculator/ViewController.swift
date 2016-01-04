bc78d0aa155763876c31a4b321fcfcf0c996322d
Kamil Pawlik <kamil.pawlik@coreo.pl>
2016-01-05 00:03:31 +0100

    Initial commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Kamil Pawlik on 04.01.2016.
+//  Copyright © 2016 COREO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billValue: UITextField!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var tipPercentSlider: UISlider!
+    @IBOutlet weak var tipValue: UILabel!
+    @IBOutlet weak var totalValue: UILabel!
+    
+    
+    var currentTipPercentValue = 20
+    var currentBillValue = Float(0.0)
+    
+    var tipCalculatorModel = TipCalculatorModel()
+    
+
+    @IBAction func tipPercentSliderChanged(sender: UISlider) {
+        currentTipPercentValue = Int(sender.value)
+        tipPercentLabel.text = "\(currentTipPercentValue)%"
+        
+        updateCalculations()
+    }
+    
+    @IBAction func billValueChanged(sender: UITextField) {
+        if let currentValue = sender.text {
+            currentBillValue = Float(currentValue)!
+            updateCalculations()
+        }
+    }
+
+    func updateCalculations() {
+        tipCalculatorModel.billValue = currentBillValue
+        tipCalculatorModel.tipPercent = currentTipPercentValue
+        
+        tipValue.text = "\(tipCalculatorModel.tipValue)zł"
+        totalValue.text = "\(tipCalculatorModel.totalValue)zł"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCalculations()
+    }
+
+    
+
+
+
+}
+
---
+++ TipCalculatorModel.swift
@@ -0,0 +1,26 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Kamil Pawlik on 04.01.2016.
+//  Copyright © 2016 COREO. All rights reserved.
+//
+
+class TipCalculatorModel {
+    
+    var billValue = Float(0.0)
+    var tipPercent = 20
+    
+    var tipValue: Int {
+        get {
+            return Int(billValue) * tipPercent / 100
+        }
+    }
+    
+    var totalValue: Float {
+        get {
+            return billValue + Float(tipValue)
+        }
+    }
+    
+}
